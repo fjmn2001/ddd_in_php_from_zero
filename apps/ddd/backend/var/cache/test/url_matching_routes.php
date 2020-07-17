ac46b6c8f8aa4b18eff232f58823790a83de3bf2
Francisco Marcano <fjmn2001@gmail.com>
2020-07-16 21:57:01 -0400

    Adding health check flow v5

---
+++ apps/ddd/backend/var/cache/test/url_matching_routes.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/health-check' => [[['_route' => 'health-check_get', '_controller' => 'MN\\Apps\\Mooc\\Ddd\\Controller\\HealthCheck\\HealthCheckGetController'], null, ['GET' => 0], null, false, false, null]],
+    ],
+    [ // $regexpList
+    ],
+    [ // $dynamicRoutes
+    ],
+    null, // $checkCondition
+];
