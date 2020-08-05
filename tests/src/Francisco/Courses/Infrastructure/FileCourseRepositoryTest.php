98a6e8663b2ac4fe319e81152f28dfc632900913
Francisco Marcano <fjmn2001@gmail.com>
2020-08-05 16:47:23 -0400

    adding value objects

--- tests/src/Francisco/Courses/Infrastructure/FileCourseRepositoryTest.php
+++ tests/src/Francisco/Courses/Infrastructure/FileCourseRepositoryTest.php
@@ -8,6 +8,9 @@ namespace MN\Tests\Francisco\Courses\Infrastructure;
 
 
 use MN\Francisco\Courses\Domain\Course;
+use MN\Francisco\Courses\Domain\CourseDuration;
+use MN\Francisco\Courses\Domain\CourseId;
+use MN\Francisco\Courses\Domain\CourseName;
 use MN\Francisco\Courses\Infrastructure\FileCourseRepository;
 use PHPUnit\Framework\TestCase;
 
@@ -19,7 +22,7 @@ final class FileCourseRepositoryTest extends TestCase
     public function it_should_save_a_course(): void
     {
         $repository = new FileCourseRepository();
-        $course = new Course('id', 'name', 'duration');
+        $course = new Course(CourseId::random(), new CourseName('name'), new CourseDuration('duration'));
 
         $repository->save($course);
     }
@@ -29,9 +32,9 @@ final class FileCourseRepositoryTest extends TestCase
      */
     public function it_should_return_an_existing_course(): void
     {
-        $course_id = (string)rand(1, 100);
+        $course_id = CourseId::random();
         $repository = new FileCourseRepository();
-        $course = new Course($course_id, 'name', 'duration');
+        $course = new Course($course_id, new CourseName('name'), new CourseDuration('duration'));
 
         $repository->save($course);
 
@@ -43,7 +46,8 @@ final class FileCourseRepositoryTest extends TestCase
      */
     public function it_should_not_return_an_existing_course(): void
     {
+        $course_id = CourseId::random();
         $repository = new FileCourseRepository();
-        $this->assertNull($repository->search('no_exists_course'));
+        $this->assertNull($repository->search($course_id));
     }
 }
