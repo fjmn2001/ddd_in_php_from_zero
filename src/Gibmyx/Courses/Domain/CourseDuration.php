79c67ff52555d49ddedb5cac9f21404f6887cd81
gibmyx <gibmyxjobs@gmail.com>
2020-08-05 17:48:59 -0400

    fix:111

--- src/Gibmyx/Courses/Domain/CourseDuration.php
+++ src/Gibmyx/Courses/Domain/CourseDuration.php
@@ -11,5 +11,16 @@ use MN\Shared\Domain\ValueObject\StringValueObject;
 
 final class CourseDuration extends StringValueObject
 {
+    public function __construct(string $value)
+    {
+        $this->ensureLengthIsInferiorThan30Characters($value);
+        parent::__construct($value);
+    }
 
+    private function ensureLengthIsInferiorThan30Characters(string $value) :void
+    {
+        if(strlen($value) > 30){
+            throw new \InvalidArgumentException(sprintf("<%s>", $value));
+        }
+    }
 }
