822921038fa3a6fe06852bb7043c3d57d0e6a941
Daniel Marcano <dmarcanor2@gmail.com>
2020-08-05 17:40:21 -0400

    Validations for CourseName

--- src/Daniel/Courses/Domain/CourseName.php
+++ src/Daniel/Courses/Domain/CourseName.php
@@ -10,5 +10,17 @@ use MN\Shared\Domain\ValueObject\StringValueObject;
 
 final class CourseName extends StringValueObject
 {
+    public function __construct(string $value)
+    {
+        $this->ensureLengthIsInferiorThan30Characters($value);
 
+        parent::__construct($value);
+    }
+
+    private function ensureLengthIsInferiorThan30Characters(string $value): void
+    {
+        if (strlen($value) > 30) {
+            throw new \InvalidArgumentException(sprintf("<%s>", $value));
+        }
+    }
 }
