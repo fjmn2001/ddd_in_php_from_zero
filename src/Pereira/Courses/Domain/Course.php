6578ed423f5d332b9e223e8f4671a0507656d82c
josepereira08 <jpyftsunami08@gmail.com>
2020-08-05 16:40:02 -0400

    commit 02

---
+++ src/Pereira/Courses/Domain/Course.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: trabajo
+ * Date: 01/08/20
+ * Time: 11:21 AM
+ */
+
+namespace MN\Pereira\Courses\Domain;
+
+
+class Course
+{
+
+    private $id;
+    private $name;
+    private $duration;
+
+
+    public function __construct(string $id, string $name, string $duration)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->duration = $duration;
+
+    }
+
+    public function id(): string
+    {
+        return $this->id;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function duration(): string
+    {
+        return $this->duration;
+    }
+}
