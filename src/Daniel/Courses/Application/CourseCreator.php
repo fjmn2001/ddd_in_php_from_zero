3489d6a464a8ecf734d453c2fafca47dead73925
Daniel Marcano <dmarcanor2@gmail.com>
2020-08-05 16:47:56 -0400

    Adding value objects

--- src/Daniel/Courses/Application/CourseCreator.php
+++ src/Daniel/Courses/Application/CourseCreator.php
@@ -8,6 +8,9 @@ namespace MN\Daniel\Courses\Application;
 
 
 use MN\Daniel\Courses\Domain\Course;
+use MN\Daniel\Courses\Domain\CourseDuration;
+use MN\Daniel\Courses\Domain\CourseId;
+use MN\Daniel\Courses\Domain\CourseName;
 use MN\Daniel\Courses\Domain\CourseRepository;
 
 final class CourseCreator
@@ -21,7 +24,9 @@ final class CourseCreator
 
     public function __invoke(CreateCourseRequest $request): void
     {
-        $course = new Course($request->id(), $request->name(), $request->duration());
+        $course = new Course(new CourseId($request->id()),
+                            new CourseName($request->name()),
+                            new CourseDuration($request->duration()));
 
         $this->repository->save($course);
     }
