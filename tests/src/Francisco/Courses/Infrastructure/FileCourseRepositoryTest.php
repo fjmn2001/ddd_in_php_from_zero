50a0be2e798db70b54a6e242c4d17873d1c2b353
Francisco Marcano <fjmn2001@gmail.com>
2020-08-12 16:05:46 -0400

    adding shared files v4

--- tests/src/Francisco/Courses/Infrastructure/FileCourseRepositoryTest.php
+++ tests/src/Francisco/Courses/Infrastructure/FileCourseRepositoryTest.php
@@ -23,10 +23,7 @@ final class FileCourseRepositoryTest extends TestCase
     public function it_should_save_a_course(): void
     {
         $repository = new FileCourseRepository();
-
         $course = CourseMother::random();
-        //$course = new Course(new CourseId(CourseId::random()->value()), new CourseName('name'), new CourseDuration('duration'));
-
         $repository->save($course);
     }
 
@@ -35,13 +32,11 @@ final class FileCourseRepositoryTest extends TestCase
      */
     public function it_should_return_an_existing_course(): void
     {
-        $course_id = CourseId::random();
         $repository = new FileCourseRepository();
-        $course = new Course(new CourseId($course_id->value()), new CourseName('name'), new CourseDuration('duration'));
-
+        $course = CourseMother::random();
         $repository->save($course);
 
-        $this->assertEquals($course, $repository->search(new CourseId($course_id->value())));
+        $this->assertEquals($course, $repository->search($course->id()));
     }
 
     /**
@@ -49,8 +44,8 @@ final class FileCourseRepositoryTest extends TestCase
      */
     public function it_should_not_return_an_existing_course(): void
     {
-        $course_id = CourseId::random();
+        $course = CourseMother::random();
         $repository = new FileCourseRepository();
-        $this->assertNull($repository->search(new CourseId($course_id->value())));
+        $this->assertNull($repository->search($course->id()));
     }
 }
