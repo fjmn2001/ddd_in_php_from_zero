2aa054ce6ea07b0c6a2f484b8fbcc21ed812856f
gibmyx <gibmyxjobs@gmail.com>
2020-08-12 16:20:30 -0400

    fix: verde1

--- tests/src/Gibmyx/Courses/Application/CourseCreatorTest.php
+++ tests/src/Gibmyx/Courses/Application/CourseCreatorTest.php
@@ -14,6 +14,7 @@ use MN\Gibmyx\Courses\Domain\CourseDuration;
 use MN\Gibmyx\Courses\Domain\CourseId;
 use MN\Gibmyx\Courses\Domain\CourseName;
 use MN\Gibmyx\Courses\Domain\CourseRepository;
+use MN\Tests\Gibmyx\Courses\Domain\CourseMother;
 use PHPUnit\Framework\TestCase;
 
 final class CourseCreatorTest extends TestCase
@@ -25,15 +26,13 @@ final class CourseCreatorTest extends TestCase
     {
         $repository = $this->createMock(CourseRepository::class);
         $creator = new CourseCreator($repository);
-
-        $course_id  = CourseId::random();
-        $name       = "some-name";
-        $duration   = "some-duration";
-
-        $course = new Course( new CourseId($course_id->value()), new CourseName($name), new CourseDuration($duration) );
-
+        $course = CourseMother::random();
         $repository->method('save')->with($course);
 
-        $creator->__invoke( new CreateCourseRequest($course_id->value(), $name, $duration) );
+        $creator->__invoke( new CreateCourseRequest(
+            $course->id()->value(),
+            $course->name()->value(),
+            $course->duration()->value())
+        );
     }
 }
