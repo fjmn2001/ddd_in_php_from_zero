e8a0c8800d060742a645f466b3036808771eecda
Chmalo <chmalo.f@gmail.com>
2020-08-12 13:00:36 -0400

    pruebas behat y phpunit

--- tests/src/Christian/Courses/Application/CourseCreatorTest.php
+++ tests/src/Christian/Courses/Application/CourseCreatorTest.php
@@ -10,6 +10,9 @@ namespace MN\Tests\Christian\Courses\Application;
 use MN\Christian\Courses\Application\CourseCreator;
 use MN\Christian\Courses\Application\CreateCourseRequest;
 use MN\Christian\Courses\Domain\Course;
+use MN\Christian\Courses\Domain\CourseDuration;
+use MN\Christian\Courses\Domain\CourseId;
+use MN\Christian\Courses\Domain\CourseName;
 use MN\Christian\Courses\Domain\CourseRepository;
 use PHPUnit\Framework\TestCase;
 
@@ -23,13 +26,13 @@ final class CourseCreatorTest extends TestCase
         $repository = $this->createMock(CourseRepository::class);
         $creator = new CourseCreator($repository);
 
-        $id = 'some-id';
+        $course_id = CourseId::random();
         $name = 'some-name';
         $duration = 'some-duration';
 
-        $course = new Course($id, $name, $duration);
+        $course = new Course(new CourseId($course_id->value()), new CourseName($name), new CourseDuration($duration));
         $repository->method('save')->with($course);
 
-        $creator->__invoke(new CreateCourseRequest($id, $name, $duration));
+        $creator->__invoke(new CreateCourseRequest($course_id->value(), $name, $duration));
     }
 }
