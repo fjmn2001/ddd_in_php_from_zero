845cd7939ec2027ff6dedb7f9f23eb9665fedb3a
Francisco Marcano <fjmn2001@gmail.com>
2020-08-12 22:08:39 -0400

    fix test

--- tests/src/Francisco/Courses/Infrastructure/FileCourseRepositoryTest.php
+++ tests/src/Francisco/Courses/Infrastructure/FileCourseRepositoryTest.php
@@ -7,22 +7,18 @@ declare(strict_types=1);
 namespace MN\Tests\Francisco\Courses\Infrastructure;
 
 
-use MN\Francisco\Courses\Domain\Course;
-use MN\Francisco\Courses\Domain\CourseDuration;
-use MN\Francisco\Courses\Domain\CourseId;
-use MN\Francisco\Courses\Domain\CourseName;
 use MN\Francisco\Courses\Infrastructure\FileCourseRepository;
+use MN\Tests\Francisco\Courses\CoursesModuleUnitTestCase;
 use MN\Tests\Francisco\Courses\Domain\CourseMother;
-use PHPUnit\Framework\TestCase;
 
-final class FileCourseRepositoryTest extends TestCase
+final class FileCourseRepositoryTest extends CoursesModuleUnitTestCase
 {
     /**
      * @test
      */
     public function it_should_save_a_course(): void
     {
-        $repository = new FileCourseRepository();
+        $repository = new FileCourseRepository;
         $course = CourseMother::random();
         $repository->save($course);
     }
@@ -32,7 +28,7 @@ final class FileCourseRepositoryTest extends TestCase
      */
     public function it_should_return_an_existing_course(): void
     {
-        $repository = new FileCourseRepository();
+        $repository = new FileCourseRepository;
         $course = CourseMother::random();
         $repository->save($course);
 
@@ -44,8 +40,8 @@ final class FileCourseRepositoryTest extends TestCase
      */
     public function it_should_not_return_an_existing_course(): void
     {
+        $repository = new FileCourseRepository;
         $course = CourseMother::random();
-        $repository = new FileCourseRepository();
         $this->assertNull($repository->search($course->id()));
     }
 }
