4d93daf021b1e2b7e1abe56b7255c3fc66e39966
josepereira08 <jpyftsunami08@gmail.com>
2020-08-19 14:58:27 -0400

    res04

--- src/Pereira/Courses/Infrastructure/Persistence/DoctrineCourseRepository.php
+++ src/Pereira/Courses/Infrastructure/Persistence/DoctrineCourseRepository.php
@@ -7,15 +7,18 @@ namespace MN\Pereira\Courses\Infrastructure\Persistence;
 use MN\Pereira\Courses\Domain\Course;
 use MN\Pereira\Courses\Domain\CourseId;
 use MN\Pereira\Courses\Domain\CourseRepository;
+use MN\Shared\Infrastructure\Persistence\Doctrine\DoctrineRepository;
 
 
 final class DoctrineCourseRepository extends DoctrineRepository implements CourseRepository
 {
-    public function save(Course $course)
+
+    public function save(Course $course): void
     {
-        $this->persist();
+        $this->persist($course);
     }
-    public function search( CourseId $id):Course
+
+    public function search(CourseId $id): ?Course
     {
         return $this->repository(Course::class)->find($id);
     }
