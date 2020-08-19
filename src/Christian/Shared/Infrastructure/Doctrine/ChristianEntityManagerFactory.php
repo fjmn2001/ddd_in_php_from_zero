98c171a88efaa8c79c583af174dfe79661ef56b6
Chmalo <chmalo.f@gmail.com>
2020-08-19 15:39:20 -0400

    taller v3

---
+++ src/Christian/Shared/Infrastructure/Doctrine/ChristianEntityManagerFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MN\Christian\Shared\Infrastructure\Doctrine;
+
+final class ChristianEntityManagerFactory
+{
+    private const SCHEMA_PATH = __DIR__. '/../../../../../database/christian.sql';
+
+    public static function create(array $parameters, string $environment): EntityManagerInterface
+    {
+        $isDevMode = 'prod' !== $environment;
+
+        $prefixes = array_merge(
+            DoctrinePrefixesSearcher::inPath(DIR . '/../../../../Christian', 'MN\Christian')
+        );
+
+        $dbalCustomTypesClasses = DbalTypeSearcher::inPath(DIR . '/../../../../Christian', 'Christian');
+
+        return DoctrineEntityManagerFactory::create(
+            $parameters,
+            $prefixes,
+            $isDevMode,
+            self::SCHEMA_PATH,
+            $dbalCustomTypesClasses
+        );
+    }
+}
