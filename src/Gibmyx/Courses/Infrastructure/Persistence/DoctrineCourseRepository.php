56bd751b29d1de91635d0902849e2f490217b06a
gibmyx <gibmyxjobs@gmail.com>
2020-08-19 14:38:16 -0400

    fix: 1

---
+++ src/Gibmyx/Courses/Infrastructure/Persistence/DoctrineCourseRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+
+declare(strict_types=1);
+
+
+namespace MN\Gibmyx\Courses\Infrastructure\Persistence;
+
+
+use MN\Gibmyx\Courses\Domain\Course;
+use MN\Gibmyx\Courses\Domain\CourseId;
+use MN\Gibmyx\Courses\Domain\CourseRepository;
+
+final class DoctrineCourseRepository extends DoctrineRepository implements CourseRepository
+{
+
+    public function save(Course $course): void
+    {
+        $this->persist($course);
+    }
+
+    public function search(CourseId $id): ?Course
+    {
+        return $this->repository(Course::class)->find($id);
+    }
+}
