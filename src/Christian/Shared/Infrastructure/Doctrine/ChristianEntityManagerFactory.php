dd0e43956bc98e8eb5061b75218deb4e27c04fff
Chmalo <chmalo.f@gmail.com>
2020-08-19 16:10:21 -0400

    taller v5

--- src/Christian/Shared/Infrastructure/Doctrine/ChristianEntityManagerFactory.php
+++ src/Christian/Shared/Infrastructure/Doctrine/ChristianEntityManagerFactory.php
@@ -8,7 +8,7 @@ use MN\Shared\Infrastructure\Doctrine\DoctrineEntityManagerFactory;
 
 final class ChristianEntityManagerFactory
 {
-    private const SCHEMA_PATH = __DIR__. '/../../../../../database/christian.sql';
+    private const SCHEMA_PATH = __DIR__. '/../../../../../databases/christian.sql';
 
     public static function create(array $parameters, string $environment): EntityManagerInterface
     {
