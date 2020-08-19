7734b185d5b508e68e2ccc24095c7bd6a1544038
Chmalo <chmalo.f@gmail.com>
2020-08-19 15:08:17 -0400

    taller v3

--- src/Christian/Courses/Infrastructure/Persistence/Doctrine/CourseIdType.php
+++ src/Christian/Courses/Infrastructure/Persistence/Doctrine/CourseIdType.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace MN\Christian\Courses\Infrastructure\Persistence\Doctrine;
 
+use MN\Christian\Courses\Domain\CourseId;
+use MN\Shared\Infrastructure\Persistence\Doctrine\UuidType;
+
 final class CourseIdType extends UuidType
 {
     public static function customTypeName(): string
     {
+        return 'course_id';
+    }
 
+    protected function typeClassName(): string
+    {
+        return CourseId::class;
     }
 }
