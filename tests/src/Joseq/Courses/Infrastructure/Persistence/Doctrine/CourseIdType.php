ebf7bf97f767d0471e5dad01c591ae7afeda5561
Jose Quilarque <josequiloz@gmail.com>
2020-08-19 15:36:56 -0400

    V3

--- tests/src/Joseq/Courses/Infrastructure/Persistence/Doctrine/CourseIdType.php
+++ tests/src/Joseq/Courses/Infrastructure/Persistence/Doctrine/CourseIdType.php
@@ -6,7 +6,19 @@ declare(strict_types=1);
 namespace MN\Tests\Joseq\Courses\Infrastructure\Persistence\Doctrine;
 
 
+use MN\JoseQ\Courses\Domain\CourseId;
+use MN\Shared\Infrastructure\Persistence\Doctrine\UuidType;
+
 final class CourseIdType extends UuidType
 {
 
+    public static function customTypeName(): string
+    {
+       return 'course_id';
+    }
+
+    protected function typeClassName(): string
+    {
+        return CourseId::class;
+    }
 }
---
+++ tests/src/Joseq/Shared/Infrastructure/Doctrine/JoseQEntityManagerFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace MN\Tests\Joseq\Shared\Infrastructure\Doctrine;
+
+
+use Doctrine\ORM\EntityManagerInterface;
+
+final class JoseQEntityManagerFactory
+{
+    private const SCHEMA_PATH = __DIR__ . '/../../../../../database/joseq.sql';
+
+    public static function create(array $parameters, string $enviroment) : EntityManagerInterface
+    {
+        $isDevMode = 'prod' !== $enviroment;
+        $prefixes = array_merge(
+            DoctrinePrefixesSearcher::inPath(__DIR__ . '/../../../../Joseq', 'MN\Joseq')
+        );
+
+        $dbalCustomTypeClasses = DbalTypeSearcher::inPath(__DIR__ . '/../../../../Joseq', 'Joseq');
+
+        return DoctrineManagerFactory::create(
+            $parameters,
+            $prefixes,
+            $isDevMode,
+            self::SCHEMA_PATH,
+            $dbalCustomTypeClasses
+        );
+    }
+}
