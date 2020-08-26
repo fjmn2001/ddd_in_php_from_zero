656767010c210793f0f5c68e7f0a9e6d55c305d0
Jose Quilarque <josequiloz@gmail.com>
2020-08-26 17:38:14 -0400

    New changes

--- tests/src/Joseq/Courses/CourseModuleInfrastructureTestCase.php
+++ tests/src/Joseq/Courses/CourseModuleInfrastructureTestCase.php
@@ -6,13 +6,15 @@ declare(strict_types=1);
 namespace MN\Tests\Joseq\Courses;
 
 
+use Doctrine\ORM\EntityManager;
 use MN\JoseQ\Courses\Domain\CourseRepository;
+use MN\JoseQ\Courses\Infrastructure\Persistence\DoctrineCourseRepository;
 use MN\Tests\Joseq\Shared\Infrastructure\PhpUnit\JoseQContextInfrastructureTestCase;
 
 abstract class CourseModuleInfrastructureTestCase extends JoseQContextInfrastructureTestCase
 {
-    protected function repository(): CourseRepository
+    protected function doctrineRepository(): CourseRepository
     {
-        return $this->service(CourseRepository::class);
+        return new DoctrineCourseRepository($this->service(EntityManager::class));
     }
 }
--- tests/src/Joseq/Courses/Infrastructure/Persistence/DoctrineCourseRepositoryTest.php
+++ tests/src/Joseq/Courses/Infrastructure/Persistence/DoctrineCourseRepositoryTest.php
@@ -17,6 +17,25 @@ final class DoctrineCourseRepositoryTest extends CourseModuleInfrastructureTestC
     public function try_it_should_save_a_course()
     {
         $course = CourseMother::random();
-        $this->repository()->save($course);
+        $this->doctrineRepository()->save($course);
+        $this->clearUnitOfWork();
+    }
+    /**
+     * @test
+     */
+    public function it_should_return_an_existing_course(): void
+    {
+        $course = CourseMother::random();
+        $this->doctrineRepository()->save($course);
+        $this->assertEquals($course, $this->doctrineRepository()->search($course->id()));
+        $this->clearUnitOfWork();
+    }
+    /**
+     * @test
+     */
+    public function it_should__not_return_an_existing_course(): void
+    {
+        $course = CourseMother::random();
+        $this->assertNull($this->doctrineRepository()->search($course->id()));
     }
 }
