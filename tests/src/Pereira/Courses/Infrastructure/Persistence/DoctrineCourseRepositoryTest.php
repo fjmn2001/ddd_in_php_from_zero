1616edab1dad0129ac17c877c93f07740d093af2
josepereira08 <jpyftsunami08@gmail.com>
2020-08-26 17:03:16 -0400

    ultimo1

--- tests/src/Pereira/Courses/Infrastructure/Persistence/DoctrineCourseRepositoryTest.php
+++ tests/src/Pereira/Courses/Infrastructure/Persistence/DoctrineCourseRepositoryTest.php
@@ -1,11 +1,23 @@
 <?php
 
+
 declare(strict_types=1);
+
 
 namespace MN\Tests\Pereira\Courses\Infrastructure\Persistence;
 
 
-final class DoctrineCourseRepositoryTest
-{
+use MN\Tests\Pereira\Courses\CoursesModuleInfrastructureTestCase;
+use MN\Tests\Pereira\Courses\Domain\CourseMother;
 
+final class DoctrineCourseRepositoryTest extends CoursesModuleInfrastructureTestCase
+{
+    /**
+     * @test
+     */
+    public function try_it_should_save_a_course()
+    {
+        $course = CourseMother::random();
+        $this->repository()->save($course);
+    }
 }
--- tests/src/Pereira/Shared/infrastructure/PhpUnit/PereiraContextInfrastructureTestCase.php
+++ tests/src/Pereira/Shared/infrastructure/PhpUnit/PereiraContextInfrastructureTestCase.php
@@ -1,13 +1,35 @@
 <?php
 
 
-namespace MN\Tests\Pereira\Shared\infrastructure\PhpUnit;
+declare(strict_types=1);
 
 
-class PereiraContextInfrastructureTestCase
+namespace MN\Tests\Pereira\Shared\Infrastructure\PhpUnit;
+
+
+use Doctrine\ORM\EntityManager;
+use MN\Apps\Pereira\Backend\PereiraBackendKernel;
+use MN\Tests\Shared\Infrastructure\PhpUnit\InfrastructureTestCase;
+
+abstract class PereiraContextInfrastructureTestCase extends InfrastructureTestCase
 {
-    protected function keranelClass(): string
+    protected function setUp(): void
     {
+        parent::setUp();
+        $arranger = new PereiraEnvironmentArranger($this->service(EntityManager::class));
+        $arranger->arrange();
+    }
 
+    protected function tearDown(): void
+    {
+        $arranger = new PereiraEnvironmentArranger($this->service(EntityManager::class));
+        $arranger->close();
+
+        parent::tearDown();
+    }
+
+    protected function kernelClass(): string
+    {
+        return PereiraBackendKernel::class;
     }
 }
