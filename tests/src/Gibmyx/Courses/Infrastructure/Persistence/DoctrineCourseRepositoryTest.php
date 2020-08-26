7232839932967eb644804dc8655d96222ed537ec
gibmyx <gibmyxjobs@gmail.com>
2020-08-26 15:46:27 -0400

    fix: a

---
+++ tests/src/Gibmyx/Courses/Infrastructure/Persistence/DoctrineCourseRepositoryTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+declare(strict_types=1);
+
+
+namespace MN\Tests\Gibmyx\Courses\Infrastructure\Persistence;
+
+
+use PHPUnit\Framework\TestCase;
+
+final class DoctrineCourseRepositoryTest extends TestCase
+{
+
+}
