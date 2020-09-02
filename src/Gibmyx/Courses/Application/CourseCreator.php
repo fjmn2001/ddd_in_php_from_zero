7b88dae27f63d48f78169b20292be6c1f2516efc
gibmyx <gibmyxjobs@gmail.com>
2020-09-02 15:27:20 -0400

    fxasd

--- src/Gibmyx/Courses/Application/CourseCreator.php
+++ src/Gibmyx/Courses/Application/CourseCreator.php
@@ -12,15 +12,18 @@ use MN\Gibmyx\Courses\Domain\CourseDuration;
 use MN\Gibmyx\Courses\Domain\CourseId;
 use MN\Gibmyx\Courses\Domain\CourseName;
 use MN\Gibmyx\Courses\Domain\CourseRepository;
+use MN\Shared\Domain\Bus\Event\EventBus;
 
 class CourseCreator
 {
 
     private $reposirtory;
+    private $bus;
 
-    public function __construct(CourseRepository $reposirtory)
+    public function __construct(CourseRepository $reposirtory, EventBus $bus)
     {
         $this->reposirtory = $reposirtory;
+        $this->bus = $bus;
     }
 
     public function __invoke(CreateCourseRequest $request) :void
@@ -32,5 +35,6 @@ class CourseCreator
         );
 
         $this->reposirtory->save($course);
+        $this->bus->publish($course->pullDomainEvents());
     }
 }
