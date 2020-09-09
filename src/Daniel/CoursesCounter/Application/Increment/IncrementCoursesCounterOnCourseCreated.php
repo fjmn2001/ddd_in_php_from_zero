e89d88cc1c2c0a2f9c44bcdec7c5d52f78de5d6d
Daniel Marcano <dmarcanor2@gmail.com>
2020-09-09 17:44:20 -0400

    Fix v4

---
+++ src/Daniel/CoursesCounter/Application/Increment/IncrementCoursesCounterOnCourseCreated.php
@@ -0,0 +1,35 @@
+<?php
+
+
+declare(strict_types=1);
+
+
+namespace MN\Daniel\CoursesCounter\Application\Increment;
+
+
+use MN\Daniel\Courses\Domain\CourseCreatedDomainEvent;
+use MN\Daniel\Courses\Domain\CourseId;
+use MN\Shared\Domain\Bus\Event\DomainEventSubscriber;
+use function Lambdish\Phunctional\apply;
+
+final class IncrementCoursesCounterOnCourseCreated implements DomainEventSubscriber
+{
+    private $incrementer;
+
+    public function __construct(CoursesCounterIncrementer $incrementer)
+    {
+        $this->incrementer = $incrementer;
+    }
+
+    public static function subscribedTo(): array
+    {
+        return [CourseCreatedDomainEvent::class];
+    }
+
+    public function __invoke(CourseCreatedDomainEvent $event): void
+    {
+        $courseId = new CourseId($event->aggregateId());
+
+        apply($this->incrementer, [$courseId]);
+    }
+}
