9724b84e2a0634faa8ee5f52b3a592ae198d50ab
Chmalo <chmalo.f@gmail.com>
2020-09-09 17:23:57 -0400

    cambios1

--- src/Christian/CoursesCounter/Infrastructure/Persistence/DoctrineCoursesCounterRepository.php
+++ src/Christian/CoursesCounter/Infrastructure/Persistence/DoctrineCoursesCounterRepository.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace MN\Christian\CoursesCounter\Infrastructure\Persistence;
+
 use MN\Christian\CoursesCounter\Domain\CoursesCounterRepository;
 use \MN\Christian\CoursesCounter\Domain\CoursesCounter;
 use MN\Shared\Infrastructure\Persistence\Doctrine\DoctrineRepository;
