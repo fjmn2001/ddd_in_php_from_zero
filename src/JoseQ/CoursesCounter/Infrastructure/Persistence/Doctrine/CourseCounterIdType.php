9fe3d7429d7980d22f8a252f34fc19ce73560a2e
Jose Quilarque <josequiloz@gmail.com>
2020-09-09 17:18:46 -0400

    Jose Changes/09/10/2020

--- src/JoseQ/CoursesCounter/Infrastructure/Persistence/Doctrine/CourseCounterIdType.php
+++ src/JoseQ/CoursesCounter/Infrastructure/Persistence/Doctrine/CourseCounterIdType.php
@@ -6,9 +6,13 @@ declare(strict_types=1);
 namespace MN\JoseQ\CoursesCounter\Infrastructure\Persistence\Doctrine;
 
 
+use MN\JoseQ\CoursesCounter\Domain\CoursesCounterId;
 use MN\Shared\Infrastructure\Persistence\Doctrine\UuidType;
 
 final class CourseCounterIdType extends UuidType
 {
-
+    protected function typeClassName(): string
+    {
+        return CoursesCounterId::class;
+    }
 }
