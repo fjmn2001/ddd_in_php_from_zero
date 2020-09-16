ef63bc0b0d05d775c7dbe5e884d6e11dfd0a18d8
gibmyx <gibmyxjobs@gmail.com>
2020-09-16 14:12:25 -0400

    fix: class

--- src/Gibmyx/CoursesCounter/Infrastructure/Persistence/Doctrine/CourseCounterIdType.php
+++ src/Gibmyx/CoursesCounter/Infrastructure/Persistence/Doctrine/CourseCounterIdType.php
@@ -6,12 +6,13 @@ declare(strict_types=1);
 namespace MN\Gibmyx\CoursesCounter\Infrastructure\Persistence\Doctrine;
 
 
+use MN\Gibmyx\CoursesCounter\Domain\CoursesCounterId;
 use MN\Shared\Infrastructure\Persistence\Doctrine\UuidType;
 
 final class CourseCounterIdType extends UuidType
 {
     protected function typeClassName(): string
     {
-        // TODO: Implement typeClassName() method.
+        return CoursesCounterId::class;
     }
 }
