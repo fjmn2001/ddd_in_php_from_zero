122b32bcf30b1f35e19cd539b6906f43e8a4bbb9
Francisco Marcano <fjmn2001@gmail.com>
2020-09-18 20:20:07 -0400

    fix v2

--- apps/francisco/backend/src/Controller/CoursesCounter/CoursesCounterGetController.php
+++ apps/francisco/backend/src/Controller/CoursesCounter/CoursesCounterGetController.php
@@ -4,17 +4,27 @@
 namespace MN\Apps\Francisco\Backend\Controller\CoursesCounter;
 
 
+use MN\Francisco\CoursesCounter\Application\Find\FindCoursesCounterQuery;
+use MN\Francisco\CoursesCounter\Domain\CoursesCounterNotExist;
 use MN\Shared\Infrastructure\Symfony\ApiController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class CoursesCounterGetController extends ApiController
 {
     public function __invoke()
     {
-        //$response = $this->ask(new FindCoursesCounterQuery());
+        $response = $this->ask(new FindCoursesCounterQuery());
 
         return new JsonResponse([
-            'total' => 1
+            'total' => $response->total()
         ]);
+    }
+
+    protected function exceptions(): array
+    {
+        return [
+            CoursesCounterNotExist::class => Response::HTTP_NOT_FOUND,
+        ];
     }
 }
