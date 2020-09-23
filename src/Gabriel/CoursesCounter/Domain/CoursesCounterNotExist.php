2927da6312849d3c2137bb319841f4927e59215d
dmvm <dmvelasquez95@gmail.com>
2020-09-23 15:14:22 -0400

    ddd_gabriel 20200923 1

---
+++ src/Gabriel/CoursesCounter/Domain/CoursesCounterNotExist.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MN\Gabriel\CoursesCounter\Domain;
+
+final class CoursesCounterNotExist extends \RuntimeException
+{
+    public function __construct()
+    {
+        parent::__construct('The courses counter not exist');
+    }
+}
